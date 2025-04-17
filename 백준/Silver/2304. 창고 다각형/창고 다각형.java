ec88c52f0b50055dcfeb2b5c891a5a2ada64c3f9
dy102 <132787602+dy102@users.noreply.github.com>
2025-04-17 22:08:18 +0900

    [Silver II] Title: 창고 다각형, Time: 120 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -2,57 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(int[][] arr, int n) {
+    public int solution(int[] arr, int start, int end) {
+        int[] left = new int[end + 1];
+        int[] right = new int[end + 2];
+        for (int i = start; i <= end; i++) {
+            left[i] = Math.max(left[i - 1], arr[i]);
+        }
+        for (int i = end; i >= start; i--) {
+            right[i] = Math.max(right[i + 1], arr[i]);
+        }
         int answer = 0;
-        int max = 0;
-        int maxIdx = 0;
-        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
-        for (int i = 0; i < n; i++) {
-            if (max < arr[i][1]) {
-                max = arr[i][1];
-                maxIdx = i;
-            }
+        for (int i = start; i <= end; i++) {
+            answer += Math.min(left[i], right[i]);
         }
-        int savePoint = 0;
-        for (int i = 1; i <= maxIdx; i++) {
-            if (arr[i][1] >= arr[savePoint][1]) {
-                answer += (arr[i][0] - arr[i - 1][0]) * arr[i - 1][1];
-                savePoint = i;
-            } else {
-                arr[i][1] = arr[savePoint][1];
-                arr[i][0] = arr[savePoint][0];
-            }
-        }
-        savePoint = maxIdx;
-        for (int i = maxIdx + 1; i < n; i++) {
-            boolean check = true;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j][1] > arr[i][1]) {
-                    check = false;
-                    break;
-                }
-            }
-            if (check && arr[i][1] >= arr[n - 1][1] && arr[i][1] <= arr[savePoint][1]) {
-                answer += (arr[i][0] - arr[savePoint][0]) * arr[i][1];
-                savePoint = i;
-            } else {
-                arr[i][1] = arr[savePoint][1];
-                arr[i][0] = arr[savePoint][0];
-            }
-        }
-        return answer + max;
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        int[][] input = new int[n][2];
+        int[] arr = new int[1001];
         for (int i = 0; i < n; i++) {
             StringTokenizer token = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(token.nextToken());
-            arr[i][1] = Integer.parseInt(token.nextToken());
+            input[i][0] = Integer.parseInt(token.nextToken());
+            input[i][1] = Integer.parseInt(token.nextToken());
+            arr[input[i][0]] = input[i][1];
         }
-        System.out.println(T.solution(arr, n));
+        Arrays.sort(input, Comparator.comparingInt(a -> a[0]));
+        System.out.println(T.solution(arr, input[0][0], input[n - 1][0]));
     }
 }
