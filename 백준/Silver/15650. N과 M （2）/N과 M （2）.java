0462309ed791b6ddd3988b967b419771fda70d40
dy102 <132787602+dy102@users.noreply.github.com>
2025-04-21 11:47:50 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public StringBuilder solution(int n, int m) {
+        StringBuilder sb = new StringBuilder("");
+        boolean[] visited = new boolean[n + 1];
+        dfs(visited, new ArrayList<>(), n, m, 0, sb);
+        return sb;
+    }
+
+    public void dfs(boolean[] visited, List<Integer> arr, int n, int m, int idx, StringBuilder sb) {
+        if (idx == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int start = arr.isEmpty() ? 1 : arr.get(arr.size() - 1) + 1;
+        for (int i = start; i <= n; i++) {
+            visited[i] = true;
+            arr.add(i);
+            dfs(visited, arr, n, m, idx + 1, sb);
+            visited[i] = false;
+            arr.remove(idx);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(token.nextToken());
+        int m = Integer.parseInt(token.nextToken());
+        System.out.println(T.solution(n, m));
+    }
+}
