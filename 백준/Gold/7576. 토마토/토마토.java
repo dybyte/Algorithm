c67dc62fb24294ebe82d44041df3f4eec1283a09
dy102 <132787602+dybyte@users.noreply.github.com>
2025-05-17 08:26:11 +0900

    [Gold V] Title: 토마토, Time: 700 ms, Memory: 190652 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public int solution(int[][] arr, int n, int m) {
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 1) queue.offer(new int[]{i, j, 0});
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            answer = poll[2];
+            int[] dx = new int[]{-1, 0, 1, 0};
+            int[] dy = new int[]{0, -1, 0, 1};
+            for (int i = 0; i < 4; i++) {
+                int x = poll[0] + dx[i];
+                int y = poll[1] + dy[i];
+                if (x >= 0 && x < n && y >= 0 && y < m && arr[x][y] == 0) {
+                    arr[x][y] = 1;
+                    queue.offer(new int[]{x, y, answer + 1});
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) return -1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(token.nextToken());
+        int m = Integer.parseInt(token.nextToken());
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            token = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(token.nextToken());
+            }
+        }
+        System.out.println(T.solution(arr, m, n));
+    }
+}
