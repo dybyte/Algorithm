3e7a7c0787cb4f0d29fc82b110178512eb8517c2
dy102 <132787602+dybyte@users.noreply.github.com>
2025-05-27 22:31:40 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18184 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static int solution(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(Integer.MAX_VALUE, dp[i - 1] + 1);
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(solution(n));
+    }
+}
