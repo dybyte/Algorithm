57981e44cc4deddde04b9264f70b425f60f61774
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-02 21:43:15 +0900

    0002-add-two-numbers Medium 1 ms (100%) 44.2 MB (92.83%) 06-02-2025 .java

---
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers-06-02-2025-21-43-14.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode answer = new ListNode(0);
+        ListNode cur = answer;
+        int carry = 0;
+       while(l1 != null || l2 != null){
+        int num1 = l1 != null ? l1.val : 0;
+        int num2 = l2 != null ? l2.val : 0;
+
+        int sum = num1 + num2 + carry;
+        carry = sum / 10;
+
+        cur.next = new ListNode(sum % 10);
+        cur = cur.next;
+
+        if(l1!=null) l1 = l1.next;
+        if(l2!=null) l2 = l2.next;
+       }
+        if(carry != 0) cur.next = new ListNode(carry);
+        return answer.next;
+    }
+}
