b74454b740538ec5dbd832b0a30adeb3e63755cd
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-05 21:21:10 +0900

    0098-validate-binary-search-tree Medium 0 ms (100%) 43.6 MB (45.58%) 06-05-2025 .java

---
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree-06-05-2025-21-21-09.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean validate = true;
+    public boolean isValidBST(TreeNode root) {
+        dfs(root, (long)Integer.MIN_VALUE - 1, (long)Integer.MAX_VALUE + 1);
+        return validate;
+    }
+
+    void dfs(TreeNode treeNode, long less, long more){
+        if(treeNode != null){
+            if(less < treeNode.val && treeNode.val < more){
+                if(treeNode.left != null){
+                    dfs(treeNode.left, less, treeNode.val);
+                }
+                if(treeNode.right != null){
+                    dfs(treeNode.right, treeNode.val, more);
+                }
+            }
+            else {
+                validate = false;
+                return;
+            }
+        }
+    }
+}
