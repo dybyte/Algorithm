6382a46247bc5da213cbfb9c40b9db5b872d89f3
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-09 20:50:56 +0900

    0909-snakes-and-ladders Medium 62 ms (5.6%) 44.7 MB (50.82%) 06-09-2025 .java

---
+++ LeetCode/Medium/0909-snakes-and-ladders/0909-snakes-and-ladders-06-09-2025-20-50-54.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+        Map<Integer, Integer> snakes = new HashMap<>();
+        int count = 1;
+        int flag = 0;
+        for(int i = board.length - 1; i >= 0; i--){
+            if(flag%2==0){
+                for(int j = 0; j < board[0].length; j++){
+                    if(board[i][j] != -1){
+                        snakes.put(count,board[i][j]);
+                    }
+                    count++;
+                } 
+            } else{
+                for(int j = board[0].length - 1; j >= 0; j--){
+                    if(board[i][j] != -1){
+                        snakes.put(count,board[i][j]);
+                    }
+                    count++;
+                }
+            }
+            flag++;
+        }
+        Queue<Position> positions = new LinkedList<>();
+        positions.add(new Position(1, 0));
+        while (!positions.isEmpty()) {
+            Position poll = positions.poll();
+            if (poll.number >= board.length*board.length) return poll.count;
+            if (poll.count >= board.length*board.length) return -1;
+            for (int i = 1; i <= 6; i++) {
+                int num = snakes.getOrDefault(poll.number + i, poll.number + i);
+                Position position = new Position(num, poll.count + 1);
+                if (!positions.contains(position)) {
+                    positions.add(position);
+                }
+            }
+        }
+        return -1;
+    }
+     public class Position {
+        int number;
+        int count;
+
+        public Position(int number, int count) {
+            this.number = number;
+            this.count = count;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Position position = (Position) obj;
+            return this.number == position.number;
+        }
+    }
+}
