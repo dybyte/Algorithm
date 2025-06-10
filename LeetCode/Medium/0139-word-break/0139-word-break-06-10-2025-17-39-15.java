f931631bc121f06f7614e2b36410ab25a87d089d
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-10 17:39:16 +0900

    0139-word-break Medium 11 ms (8.38%) 45.2 MB (8.12%) 06-10-2025 .java

---
+++ LeetCode/Medium/0139-word-break/0139-word-break-06-10-2025-17-39-15.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < wordDict.size(); i++){
+            set.add(wordDict.get(i));    
+        }
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i = 0 ; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                if(set.contains(s.substring(i,j+1))) {
+                    if(dp[i]) {
+                        dp[j+1] = true;
+                    }
+                    
+                }
+            }
+        }
+        return dp[dp.length-1];
+    }
+}
