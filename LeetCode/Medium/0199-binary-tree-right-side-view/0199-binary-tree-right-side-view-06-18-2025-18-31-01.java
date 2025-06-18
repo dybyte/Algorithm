c7e9aa68c17db5048806ec318a0fe2ebafc24453
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-18 18:31:01 +0900

    0199-binary-tree-right-side-view Medium 1 ms (71.38%) 42.1 MB (79.18%) 06-18-2025 .java

---
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view-06-18-2025-18-31-01.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(new Node(root,0));
+        List<Integer> list = new ArrayList<>();
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            TreeNode treeNode = node.treeNode;
+            if(treeNode == null) continue;
+            int depth = node.depth;
+            
+            if(list.size() - 1 < depth) list.add(treeNode.val);
+            else list.set(depth, treeNode.val);
+           
+            queue.offer(new Node(treeNode.left, depth+1));
+            queue.offer(new Node(treeNode.right, depth+1));
+        }
+        return list;
+    }
+
+    class Node{
+        TreeNode treeNode;
+        int depth;
+        Node (TreeNode treeNode, int depth){
+            this.treeNode = treeNode;
+            this.depth = depth;
+        }
+    }
+}
