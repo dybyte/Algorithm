eec62fe72040ef7976502e5f9a07e22601f753ab
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-24 20:53:59 +0900

    0184-department-highest-salary Medium 1080 ms (15.02%) 0B (100%) 06-24-2025 .sql

---
+++ LeetCode/Medium/0184-department-highest-salary/0184-department-highest-salary-06-24-2025-20-53-58.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee, e.salary as Salary
+from Employee e
+left join Department d
+on e.departmentId = d.id
+where e.salary = (
+    select MAX(e2.salary)
+    from Employee e2
+    where e.departmentId = e2.departmentId
+);
