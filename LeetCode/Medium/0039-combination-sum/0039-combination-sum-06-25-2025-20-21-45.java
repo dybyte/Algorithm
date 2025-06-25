2d7b6005a19e21f82d7d54f9d51b720c822021f3
dy102 <132787602+dybyte@users.noreply.github.com>
2025-06-25 20:21:45 +0900

    0039-combination-sum Medium 2 ms (83.42%) 44.9 MB (31.95%) 06-25-2025 .java

---
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum-06-25-2025-20-21-45.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> answer = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        dfs(new ArrayList<>(),candidates,target,0,0);
+        return answer;
+    }
+
+    void dfs(List<Integer> arr, int[] candidates, int target, int curSum, int curIdx){
+        if(target == curSum){
+            answer.add(new ArrayList<>(arr));
+            return;
+        }
+        if(target < curSum){
+            return;
+        }
+        for(int i = curIdx; i < candidates.length; i++){
+            arr.add(candidates[i]);
+            dfs(arr,candidates,target,curSum+candidates[i],i);
+            arr.remove(arr.size()-1);
+        }
+    }
+}
