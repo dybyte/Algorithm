3d7538889f74bee3ed23916b7a95498de0d53ad4
dy102 <132787602+dybyte@users.noreply.github.com>
2025-07-01 21:32:13 +0900

    0073-set-matrix-zeroes Medium 2 ms (17.49%) 45.6 MB (63.68%) 07-01-2025 .java

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes-07-01-2025-21-32-13.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        //O(m+n) 공간복잡도
+        Map<Integer, Boolean> row = new HashMap<>();
+        Map<Integer, Boolean> col = new HashMap<>();
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    row.put(i,true);
+                    col.put(j,true);
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(row.getOrDefault(i,false) || col.getOrDefault(j,false)){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
