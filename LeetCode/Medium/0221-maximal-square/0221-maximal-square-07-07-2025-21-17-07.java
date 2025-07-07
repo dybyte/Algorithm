63a424dd71489aa3976d9512f335a42f4037de29
dy102 <132787602+dybyte@users.noreply.github.com>
2025-07-07 21:17:07 +0900

    0221-maximal-square Medium 8 ms (70.13%) 59.9 MB (80.72%) 07-07-2025 .java

---
+++ LeetCode/Medium/0221-maximal-square/0221-maximal-square-07-07-2025-21-17-07.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        // i,j 
+        // 1. i-1, j-1
+        // 2. i-1, j
+        // 3. i, j-1
+
+        int[][] arr = new int[matrix.length + 1][matrix[0].length + 1];
+        int ans = 0;
+        for(int i = 1; i < arr.length; i++){
+            for(int j = 1; j < arr[0].length; j++){
+                if(matrix[i-1][j-1] - '0' == 0) continue;
+                arr[i][j] = Math.min(arr[i-1][j-1], Math.min(arr[i-1][j], arr[i][j-1])) + 1;
+                ans = Math.max(ans, arr[i][j]);
+            }
+        }
+        return ans * ans;
+    }
+}
