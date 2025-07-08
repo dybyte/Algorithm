a48e0b3bd5692fba67710eaa67b955177f2e621d
dy102 <132787602+dybyte@users.noreply.github.com>
2025-07-08 17:51:44 +0900

    0045-jump-game-ii Medium 38 ms (15.36%) 45.7 MB (14.22%) 07-08-2025 .java

---
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii-07-08-2025-17-51-43.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 1; j <= nums[i]; j++){
+                int tmp = i+j;
+                if(tmp < nums.length){
+                    dp[tmp] = Math.min(dp[tmp], dp[i] + 1);
+                } else break;
+            }
+        }
+        return dp[nums.length-1];
+    }
+}
