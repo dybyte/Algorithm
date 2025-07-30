0411d125f0a869b7a86d2e676617acfcac522d79
dy102 <132787602+dybyte@users.noreply.github.com>
2025-07-30 21:25:33 +0900

    0329-longest-increasing-path-in-a-matrix Hard 9 ms (53.69%) 45 MB (65.74%) 07-30-2025 .java

---
+++ LeetCode/Hard/0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix-07-30-2025-21-25-32.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[][] DIRECTION = {{-1,0},{1,0},{0,-1},{0,1}};
+    public int longestIncreasingPath(int[][] matrix) {
+        //DFS + DP
+        int ans = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                dp[i][j] = dfs(matrix, i, j, dp);
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+    int dfs(int[][] matrix, int x, int y, int[][] dp){
+        if(dp[x][y]!=0) return dp[x][y];
+        int maxLen = 1;
+        for(int i = 0; i < 4; i++){
+            int nX = x + DIRECTION[i][0];
+            int nY = y + DIRECTION[i][1];
+            if(nX >= 0 && nY >= 0 && nX < matrix.length && nY < matrix[0].length
+            && matrix[nX][nY] > matrix[x][y]){
+                int len = 1 + dfs(matrix, nX, nY, dp);
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        dp[x][y] = maxLen;
+        return maxLen;
+    }
+}
