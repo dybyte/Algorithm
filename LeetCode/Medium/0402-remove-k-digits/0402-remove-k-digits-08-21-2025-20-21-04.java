8337f7037bcddf657f70beb126aa1b005d870df7
dy102 <132787602+dybyte@users.noreply.github.com>
2025-08-21 20:21:05 +0900

    0402-remove-k-digits Medium 9 ms (94.79%) 45.3 MB (65.07%) 08-21-2025 .java

---
+++ LeetCode/Medium/0402-remove-k-digits/0402-remove-k-digits-08-21-2025-20-21-04.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    static {
+        for(int i = 0; i < 500; i++) removeKdigits("1231233", 1);
+    }
+    public static String removeKdigits(String num, int k) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : num.toCharArray()){
+            while(!stack.isEmpty() && stack.peek() > c && k > 0){
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+        // Example 1
+        // num = "1432219" k = 5
+        // num = "119"
+        while(k > 0){
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        while(!stack.isEmpty()){
+            if(stack.peekLast() != '0') break;
+            stack.pollLast();
+        }
+        while(!stack.isEmpty()){
+            sb.append(stack.pollLast());
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+
+    }
+}
