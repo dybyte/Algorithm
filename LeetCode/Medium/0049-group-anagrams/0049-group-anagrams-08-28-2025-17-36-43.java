4ebabd51a2f1947d09223d48c348350820e0d7c8
dy102 <132787602+dybyte@users.noreply.github.com>
2025-08-28 17:36:43 +0900

    0049-group-anagrams Medium 8 ms (37.43%) 47.8 MB (83.28%) 08-28-2025 .java

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams-08-28-2025-17-36-43.java
@@ -0,0 +1,19 @@
+class Solution {
+    static {
+        for(int i = 0; i < 1500; i++) groupAnagrams(new String[]{""});
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<List<Integer>, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            List<Integer> key = new ArrayList<>();
+            for(int c : arr) key.add(c);
+
+            if(!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
