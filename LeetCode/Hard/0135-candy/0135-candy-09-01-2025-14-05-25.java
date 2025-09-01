31b1f4ea141debae42d0720d6efac533f295086f
dy102 <132787602+dybyte@users.noreply.github.com>
2025-09-01 14:05:26 +0900

    0135-candy Hard 175 ms (5.11%) 45.4 MB (94.43%) 09-01-2025 .java

---
+++ LeetCode/Hard/0135-candy/0135-candy-09-01-2025-14-05-25.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] counts = new int[ratings.length];
+        Arrays.fill(counts, 1);
+        int count = 0;
+        for(int i = 1; i < ratings.length; i++){
+            if(ratings[i-1] < ratings[i]){
+                counts[i] = counts[i-1] + 1;
+            }
+        }
+        for(int i = ratings.length - 2; i >= 0; i--){
+            if(ratings[i+1] < ratings[i]){
+                if(counts[i] <= counts[i+1]) {
+                    counts[i] = counts[i+1] + 1;
+                }
+            }
+        }
+        for(int i = 0; i < counts.length; i++){
+            System.out.println(counts[i]);
+            count += counts[i];
+        }
+        return count;
+    }
+}
