96aeb064a4db1378a1c6790965c415cb9841f354
dy102 <132787602+dybyte@users.noreply.github.com>
2025-09-02 18:59:19 +0900

    0177-nth-highest-salary Medium 608 ms (12.33%) 0B (100%) 09-02-2025 .sql

---
+++ LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary-09-02-2025-18-59-18.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+   DECLARE offset_value INT;
+   SET offset_value = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT IFNULL(
+        (SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET offset_value),
+        NULL)
+  );
+END
