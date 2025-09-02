f29408ee74013db18e97e9cec45433a9462dc58a
dy102 <132787602+dybyte@users.noreply.github.com>
2025-09-02 09:52:35 +0900

    0176-second-highest-salary Medium 250 ms (91.07%) 0B (100%) 09-02-2025 .sql

---
+++ LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary-09-02-2025-09-52-34.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT IFNULL(
+    (SELECT salary
+    FROM Employee
+    GROUP BY salary
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1),
+    NULL)
+AS SecondHighestSalary;
+
+
