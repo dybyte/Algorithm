716b1dfc10fa1d57b41d032d21dd24ece37c0919
dy102 <132787602+dybyte@users.noreply.github.com>
2025-09-08 19:12:54 +0900

    0550-game-play-analysis-iv Medium 1370 ms (7.46%) 0B (100%) 09-08-2025 .sql

---
+++ LeetCode/Medium/0550-game-play-analysis-iv/0550-game-play-analysis-iv-09-08-2025-19-12-53.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+WITH first_login AS (
+    SELECT player_id, MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+),
+next_day_login AS (
+    SELECT f.player_id
+    FROM first_login f
+    JOIN Activity a
+    ON f.player_id = a.player_id
+    WHERE a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+)
+SELECT ROUND(
+    (
+        SELECT COUNT(*) FROM next_day_login
+    )
+    /(
+        SELECT COUNT(DISTINCT player_id)
+        FROM Activity
+    )
+    ,2) AS fraction;
