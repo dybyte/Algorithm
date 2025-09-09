70a48bbfb3087ee333279b097ce1b21dc7ca363a
dy102 <132787602+dybyte@users.noreply.github.com>
2025-09-09 18:45:04 +0900

    0197-rising-temperature Easy 1324 ms (5.01%) 0B (100%) 09-09-2025 .sql

---
+++ LeetCode/Easy/0197-rising-temperature/0197-rising-temperature-09-09-2025-18-45-03.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT id
+FROM Weather w
+WHERE (
+    SELECT temperature
+    FROM Weather
+    WHERE recordDate = DATE_ADD(w.recordDate, INTERVAL -1 DAY)
+) < temperature;
+
