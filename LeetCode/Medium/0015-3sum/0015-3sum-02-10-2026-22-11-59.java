bb2ec07cf0f637b7c568e42dc0d78dbc74e6d6d6
dy102 <132787602+dybyte@users.noreply.github.com>
2026-02-10 22:11:59 +0900

    0015-3sum Medium 34 ms (59.35%) 59.8 MB (22.78%) 02-10-2026 .java

---
+++ LeetCode/Medium/0015-3sum/0015-3sum-02-10-2026-22-11-59.java
@@ -0,0 +1,35 @@
+class Solution {
+    static {
+        for(int i = 0; i < 1000; i++){
+            threeSum(new int[]{0,0,0});
+        }
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        Arrays.sort(nums);
+
+        for(int i = 0; i < len; ++i) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = len - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum > 0) {
+                    right--;
+                }else if(sum < 0) {
+                    left++;
+                }else {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+
+                    while(nums[left] == nums[left - 1] && left < right) left++;
+                }
+            }
+        }
+        return ans;
+    }
+}
