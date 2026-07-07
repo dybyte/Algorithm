296bd7cd2ac8c96a2bdae74751d1685626e46b8e
Doyeon Kim <132787602+dybyte@users.noreply.github.com>
2026-07-08 00:28:05 +0900

    0021-merge-two-sorted-lists Easy 0 ms (100%) 44.3 MB (38.97%) 07-08-2026 .java

---
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists-07-08-2026-00-27-58.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        /**
+        - p1: pointer of list1
+        - p2: pointer of list2
+        - curr: tail of the answer list node
+
+        1) pick the smaller node of list1 of list2
+        2) we set curr.next to smaller node
+        3) move p1 or p2 forward
+        4) set curr to curr.next
+        5) repeat this process until p1 and p2 become null
+         */
+
+        ListNode curr = new ListNode();
+        ListNode ans = curr;
+        ListNode p1 = list1, p2 = list2;
+
+        while(p1 != null && p2 != null) {
+            if(p1.val < p2.val) {
+                curr.next = p1;
+                p1 = p1.next;
+            } else {
+                curr.next = p2;
+                p2 = p2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(p1 != null) {
+            curr.next = p1;
+        } else {
+            curr.next = p2;
+        }
+
+         return ans.next;
+    }
+}
