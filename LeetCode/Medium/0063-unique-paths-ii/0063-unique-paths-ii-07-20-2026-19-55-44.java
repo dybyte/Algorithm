b3af37e5b25bf070a8c012b82641b8182d1ece93
Doyeon Kim <132787602+dybyte@users.noreply.github.com>
2026-07-20 19:55:45 +0900

    0063-unique-paths-ii Medium 0 ms (100%) 43.5 MB (64.46%) 07-20-2026 .java

---
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii-07-20-2026-19-55-44.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    static {
+        int[][] arr = {{0,0}};
+        uniquePathsWithObstacles(arr);
+    }
+
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (obstacleGrid[row][col] == 1) {
+                    dp[row][col] = 0;
+                    continue;
+                }
+
+                if (row > 0) {
+                    dp[row][col] += dp[row - 1][col];
+                }
+
+                if (col > 0) {
+                    dp[row][col] += dp[row][col - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
